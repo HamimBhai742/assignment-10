164acbfe48c37639e6087ab54942fa3fa66c0746
HamimBhai742 <omagootruelove147@gmail.com>
2024-04-25 20:11:38 +0600

    Basic enviorment setup complete

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <p>This Is Home</p>
+            <h1>This is Home</h1>
+            <h2>This is Home</h2>
+            <h3>This is Home</h3>
+            <h4>This is Home</h4>
+            <h5>This is Home</h5>
+            <h6 className='text-6xl'>This is Home</h6>
         </div>
     );
 };
